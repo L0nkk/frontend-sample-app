5c1af510e168074f39fa3b6c3311501d843c81db
nan <103703190+legn0@users.noreply.github.com>
2024-11-04 22:52:16 -0300

    v1.1.0

--- src/pages/practica_industrial.jsx
+++ src/pages/practica_industrial.jsx
@@ -1,51 +1,80 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import NavBar from '../components/nav_bar';
+import NavBar from "../components/nav_bar";
 
 export const HomePage = () => {
   const navigate = useNavigate();
-  const [showAlternativeView, setShowAlternativeView] = useState(false);
+  const [showAlternativeView, setShowAlternativeView] = useState(true);
 
   const handleTitleClick = () => {
-    navigate('/inicio'); // Navigate to the "/inicio" route when "Practica DI" is clicked
-  };
-
-  const handleConvalidarClick = () => {
-    navigate('/practica_convalidar'); // Navigate to the "/practica_convalidar"
-  };
-
-  const handleInscribirClick = () => {
-    navigate('/practica_inscribir'); // Navigate to the "/practica_inscribir"
+    navigate("/inicio"); // Navigate to the "/inicio" route when "Practica DI" is clicked
   };
 
   const toggleView = () => {
     setShowAlternativeView(!showAlternativeView);
   };
 
+  const notas = [
+    { name: "Capacidad", calificacionn: 4 },
+    { name: "Confianza", calificacionn: 5 },
+    { name: "Aplicacion y Empeño", calificacionn: 5 },
+    { name: "Adaptabilidad", calificacionn: 5 },
+    { name: "Iniciativa", calificacionn: 5 },
+    { name: "Aptitud Trabajo en Equipo", calificacionn: 5 },
+    { name: "Asistencia y Puntualidad", calificacionn: 5 },
+    { name: "Logro de Resultados", calificacionn: 3 },
+    { name: "Comunicacion Efectiva", calificacionn: 5 },
+  ];
+
   return (
     <>
       <NavBar />
-      <div className="login-container">
-        <h1 onClick={handleTitleClick} style={{ cursor: 'pointer' }}>
+      <div className="resumen-container">
+        <h1 onClick={handleTitleClick} style={{ cursor: "pointer" }}>
           Practica DI
         </h1>
         <p>Universidad Tecnico Federico Santa Maria</p>
-        <div className="base" style={{ position: 'relative' }}>
-          {/* Place the button at the top-left corner inside the base */}
-          <button onClick={toggleView} className="toggle-view-button" style={{ position: 'absolute', top: '20px', left: '20px' }}>
-            {showAlternativeView ? 'Informacion' : 'Evaluacion'}
-          </button>
+        <div className="base" style={{ position: "relative" }}>
+          <div className="toggle-view-container">
+            <button
+              onClick={toggleView}
+              className={`toggle-view-button-${
+                showAlternativeView ? "active" : "inactive"
+              }`}>
+              INFORMACIÓN
+            </button>
+            <button
+              onClick={toggleView}
+              className={`toggle-view-button-${
+                !showAlternativeView ? "active" : "inactive"
+              }`}>
+              EVALUACIÓN
+            </button>
+          </div>
 
           {/* Conditionally render Notas and Observaciones based on the alternative view state */}
           {!showAlternativeView && (
             <div className="summary-container">
               <div className="notas">
                 <h3>Notas</h3>
-                <ul>
-                  <li>Confianza: 3</li>
-                  <li>Capacidad: 4</li>
-                  <li>Iniciativa: 5</li>
+                <ul className="notas-lista">
+                  {notas.map((item, index) => {
+                    return (
+                      <li className="notas-item">
+                        {item.name}
+                        <span
+                          className="notas-item-nota"
+                          style={
+                            item.calificacionn < 4
+                              ? { color: "rgb(180, 1, 1)" }
+                              : { color: "#222" }
+                          }>
+                          {item.calificacionn}
+                        </span>
+                      </li>
+                    );
+                  })}
                 </ul>
               </div>
               <div className="observaciones">
